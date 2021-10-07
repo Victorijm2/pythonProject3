b86f51bc5faf883bb1de3597dde23a9acdea616b
Victor Juárez Macías <JM@MacBook-Pro-de-Victor.local>
2021-10-07 11:09:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# -*- coding: utf-8 -*-
+"""Ejemplo if: test primera clase"""
+
+primera_clase = False
+
+
+if(primera_clase):
+    print("Esta es mi primera clase!")
+
+else:
+    print("ya se algo de informática!")
+
+nombre = input("¿cual es tu nombre?"
+apellidos = input("apellidos")
+print(nombre, " ", apellidos)
